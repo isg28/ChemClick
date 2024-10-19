3d02c88f19e0a0fd8b0249a0a0c887b2ea9eba89
marez <121374072+mkdso27@users.noreply.github.com>
2024-10-18 21:30:01 -0700

    JIRA CHEM-81 Create Question Page UI - REVISED

--- frontend/src/components/question/Question.js
+++ frontend/src/components/question/Question.js
@@ -1,74 +1,83 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../../styles/question/Question.css';
+import { useNavigate } from 'react-router-dom';
+
 
 function Question() {
     const navigate = useNavigate();
 
-    const handleHomeClick = () => {
+    const handlequestion = () => {
         navigate('/dashboard');
     };
+
     return (
         <div className='Question'>
-            <div className='QuestionHeader'>
-                <img src={require('../../assets/question/ChemClickLogo.png')} className="Logo" />
-                <img
-                    src={require('../../assets/question/Home.png')} className="Home" onClick={handleHomeClick} style={{ cursor: 'pointer' }}
-                />
-                <div className='WhiteHeaderBox'>
-                    <h2>ChemClick Assignments</h2>
-                </div>
-            </div>
-            <div className='BlueQuestionBox'>
-                <div className='WhiteTitleBox'>
-                    <h1>Unit 3: Nomenclature:<br />
-                        Element Name
-                    </h1>
-                </div>
-                <div className='WhiteQuestionBox'>
-                    <h2 className='QuestionText'>What is the name of this element?</h2>
-                    <img src={require('../../assets/question/Hydrogen.jpg')} className='ElementImage' />
-                    <div className='AnswerContainer'>
-                        <h2 className='AnswerText'>The name of this element is:</h2>
-                        <input type="text" className="AnswerInput" />
-                    </div>
-                    <button className="SubmitButton">
-                        <h2>Submit</h2>
-                    </button>
-                </div>
+            <div className='header'>
+                <img src={require('../../assets/question/ChemClickLogo.png')} className='ChemClickLogoHeader' />
+                <img src={require('../../assets/question/Home.png')} className='homelines' onClick={handlequestion} />
 
-            </div>
-            <div className='Mastery'>
-                <div className='MasteryWhiteTitleBox'>
-                    <h1> Mastery</h1>
+                <div className='insideheader'><h1>
+                    ChemClick Assignments
+                </h1>
                 </div>
-                <div className='MasteryStarWhiteBox'>
-                    <img src={require('../../assets/question/Stars.png')} className='StarsImage' />
+            </div>
+            <div className='questionbox'>
+                <img src={require('../../assets/question/Hydrogen.jpg')} className='element' />
+
+                <div className='questionboxtitle'><h1>
+                    Unit Three: Nomenclature: <br />
+                    Name that Element
+                </h1>
+                </div>
+                <div className='questionboxquestion'><h1>
+                    What is the name of this element?
+                </h1>
+                    <input type='text' className='elemententered' placeholder='Enter element name here' />
+                </div>
+                <div className='questionboxanswer'><h1>
+                    What is the name of this element?
+                </h1>
 
                 </div>
+                <button className='submitbutton'>Submit Answer</button>
             </div>
-            <div className='Goal'>
-                <div className='GoalWhiteTitleBox'>
-                    <h1> Goal</h1>
+            <div className='masterybox'>
+                <div className='masteryboxtitle'><h1>
+                    Mastery
+                </h1>
                 </div>
-                <div className='GoalStarWhiteBox'>
-                <img src={require('../../assets/question/Checkmarks.png')} className='CheckmarksImage' />
+
+                <div className='masteryboxstars'>
+                    <img src={require('../../assets/question/Stars.png')} className='masterystars' />
 
                 </div>
             </div>
-            <div className='Progress'>
-                <div className='ProgressWhiteTitleBox'>
-                    <h1> Progress</h1>
-                </div>
-                <div className='ProgressBarWhiteBox'>
-                    <h1>Current Topic Progress: 33% </h1>
-                    <img src={require('../../assets/question/ProgressBar.jpg')} className='ProgressBarImage' img />
-                </div>
 
+            <div className='goalbox'>
+                <div className='goalboxtitle'><h1>
+                    Goal
+                </h1>
+                </div>
+                <div className='goalboxchecks'>
+                    <img src={require('../../assets/question/Checkmarks.png')} className='goalchecks' />
+
+                </div>
+            </div>
+            <div className='progressbox'>
+                <div className='progressboxtitle'><h1>
+                    Progress
+                </h1>
+                    <h2>
+                        Current Topic Progress: 33%
+                    </h2>
+                </div>
+                <div className='progressboxbar'>
+                    <img src={require('../../assets/question/ProgressBar.jpg')} className='progressbar' />
+                </div>
             </div>
         </div>
+
     );
 }
+
 export default Question;
-
-
